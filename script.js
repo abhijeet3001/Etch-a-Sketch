509e197ffa259d29773bd7a1377626e553eaeb7b
Abhijeet Singh <89379140+abhijeet3001@users.noreply.github.com>
2023-04-08 17:38:30 +0530

    Fix formatting

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const container = document.querySelector(".container");
-let gridSize=16;
+let gridSize = 16;
 
-for (let i = 0; i < gridSize*gridSize; i++) {
+for (let i = 0; i < gridSize * gridSize; i++) {
   const sq = document.createElement("div");
   sq.classList.add("square");
   sq.setAttribute("id", i);
-  if(i<gridSize) sq.classList.add("top");
-  if((i%gridSize)==gridSize-1) sq.classList.add("right");
+  if (i < gridSize) sq.classList.add("top");
+  if (i % gridSize == gridSize - 1) sq.classList.add("right");
   container.appendChild(sq);
 }
 
 const squares = document.querySelectorAll(".square");
 squares.forEach((square) => {
-    let side=(500-gridSize-1)/gridSize;
-  square.style.width = side+'px';
-  square.style.height = side+'px';
+  let side = (500 - gridSize - 1) / gridSize;
+  square.style.width = side + "px";
+  square.style.height = side + "px";
 });

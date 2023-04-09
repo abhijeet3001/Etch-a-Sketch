c1a256eb9b107f2e5121f7489f5b9a38cf9157ca
Abhijeet Singh <89379140+abhijeet3001@users.noreply.github.com>
2023-04-09 12:39:33 +0530

    Add function to make new grid

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const container = document.querySelector(".container");
 let gridSize = 16;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const sq = document.createElement("div");
-  sq.classList.add("square");
-  sq.setAttribute("id", `b${i}`);
-  if (i < gridSize) sq.classList.add("top");
-  if (i % gridSize == gridSize - 1) sq.classList.add("right");
-  container.appendChild(sq);
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const sq = document.createElement("div");
+    sq.classList.add("square");
+    sq.setAttribute("id", `b${i}`);
+    if (i < gridSize) sq.classList.add("top");
+    if (i % gridSize == gridSize - 1) sq.classList.add("right");
+    container.appendChild(sq);
+  }
+
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    let side = (500 - gridSize - 1) / gridSize;
+    square.style.width = side + "px";
+    square.style.height = side + "px";
+  });
+
+  squares.forEach((square) => square.addEventListener("mouseover", addColor));
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-  let side = (500 - gridSize - 1) / gridSize;
-  square.style.width = side + "px";
-  square.style.height = side + "px";
-});
+createGrid(gridSize);
 
 function addColor(e) {
   console.log(e.target.id);
   const box = document.querySelector(`#${e.target.id}`);
   box.style.backgroundColor = "black";
 }
-squares.forEach((square) => square.addEventListener("mouseover", addColor));
 
-function getGridSize(){
-  let newGridSize=prompt("Enter a new size of Grid");
-  gridSize=newGridSize; 
+function getGridSize() {
+  let newGridSize = prompt("Enter a new size of Grid");
+  if (newGridSize > 100) {
+    alert("Only value till 100 are supported");
+    return;
+  }
+  gridSize = newGridSize;
+  const squareDiv = document.querySelectorAll(".square");
+  // Removing the previous grid
+  squareDiv.forEach((div) => div.remove());
+  createGrid(gridSize);
 }
-function clickButton(e){
-  if(e.target.id==='grid') getGridSize();
-  if(e.target.id==='color') getColor();
-  if(e.target.id==='clear') clearCells();
+
+function clickButton(e) {
+  if (e.target.id === "grid") getGridSize();
+  if (e.target.id === "color") getColor();
+  if (e.target.id === "clear") clearCells();
 }
+
 const btn = document.querySelectorAll(".btn");
-btn.forEach((button)=>button.addEventListener('click',clickButton));
+btn.forEach((button) => button.addEventListener("click", clickButton));
